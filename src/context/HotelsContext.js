1fc366b2aeedb2d6abeffd078e36bc8e13f00ef8
Angelica <Angelica137@users.noreply.github.com>
2020-10-20 22:02:52 +0100

    create hotel context reducer

---
+++ src/context/HotelsContext.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Api from "../api";
+
+export const HotelsContext = React.createContext();
+
+const initialValue = {
+  hotels: [],
+  hotel: {},
+  loading: true,
+  error: "",
+};
+
+const reducer = (value, action) => {
+  switch (action.type) {
+    case "GET_HOTELS_SUCCESS":
+      return {
+        ...value,
+        hotels: action.payload,
+        loading: false,
+      };
+    case "GET_HOTELS_ERROR":
+      return {
+        ...value,
+        hotels: action.payload,
+        loading: false,
+      };
+    case "GET_HOTEL_SUCCESS":
+      return {
+        ...value,
+        hotel: action.payload,
+        loading: false,
+      };
+    case "GET_HOTEL_ERROR":
+      return {
+        ...value,
+        hotel: {},
+        loading: false,
+        error: action.payload,
+      };
+  }
+};
