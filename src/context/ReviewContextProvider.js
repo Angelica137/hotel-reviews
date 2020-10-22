0c9dbeafd580dfee3c29ff8e125c46261cc1fbd8
Angelica <Angelica137@users.noreply.github.com>
2020-10-22 19:06:19 +0100

    create reviews context provider with get and post

--- src/context/ReviewContextProvider.js
+++ src/context/ReviewContextProvider.js
@@ -41,3 +41,42 @@ const reducer = (value, action) => {
       return value;
   }
 };
+
+const ReviewsContextProvider = ({ children }) => {
+  const [value, dispatch] = React.useReaducer(reducer, initialValue);
+
+  const getReviewsRequest = async (id) => {
+    const result = await Api.fetchData(
+      `https://my-json-server.typicode.com/pranayfpackt/-React-Projects/hotels/${id}/reviews`
+    );
+
+    if (result.data && result.data.length) {
+      dispatch({ type: "GET_REVIEWS_SUCCESS", payload: result.data });
+    } else {
+      dispatch({ type: "GET_REVIEWS_ERROR", payload: result.error });
+    }
+  };
+
+  const addReviewRequest = async (content) => {
+    const result = await Api.postData(
+      "https://my-json-server.typicode.com/pranayfpackt/-React-Projects/hotels",
+      content
+    );
+
+    if (result.data && result.data.hasOwnProperty("id")) {
+      dispatch({ type: "ADD_REVIEW_SUCCESS", payload: content });
+    } else {
+      dispatch({ type: "ADD_REVIEW_ERROR" });
+    }
+  };
+
+  return (
+    <ReviewsContext.Provider
+      value={{ ...value, getReviewsRequest, addReviewRequest }}
+    >
+      {children}
+    </ReviewsContext.Provider>
+  );
+};
+
+export default ReviewsContextProvider;
