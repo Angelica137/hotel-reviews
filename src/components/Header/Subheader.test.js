a34806d1f411155fa7373138d97b7fd6ff0d1ab1
Angelica <Angelica137@users.noreply.github.com>
2020-10-29 21:15:28 +0000

    test subheader renders

--- src/components/Header/Subheader.test.js
+++ src/components/Header/Subheader.test.js
@@ -1,5 +1,13 @@
+import React from "react";
+import ShallowRenderer from "react-test-renderer/shallow";
+import Subheader from "./Subheader";
+
 describe("the <Subheader /> component", () => {
   it("should render", () => {
-    expect(1 + 2).toBe(3);
+    const renderer = new ShallowRenderer();
+    renderer.render(<Subheader />);
+    const component = renderer.getRenderOutput();
+
+    expect(component).toMatchSnapshot();
   });
 });
