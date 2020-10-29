3f880957bd27cd04144fdcc237929a72b21fa798
Angelica <Angelica137@users.noreply.github.com>
2020-10-29 21:29:21 +0000

    test subheader with prop

--- src/components/Header/Subheader.test.js
+++ src/components/Header/Subheader.test.js
@@ -3,9 +3,17 @@ import ShallowRenderer from "react-test-renderer/shallow";
 import Subheader from "./Subheader";
 
 describe("the <Subheader /> component", () => {
+  const renderer = new ShallowRenderer();
+
   it("should render", () => {
-    const renderer = new ShallowRenderer();
     renderer.render(<Subheader />);
+    const component = renderer.getRenderOutput();
+
+    expect(component).toMatchSnapshot();
+  });
+
+  it("should render with a dynamic title", () => {
+    renderer.render(<Subheader title="Test Application" />);
     const component = renderer.getRenderOutput();
 
     expect(component).toMatchSnapshot();
